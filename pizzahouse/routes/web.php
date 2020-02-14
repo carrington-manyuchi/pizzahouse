731f9a97da56301712e0b13b75c7fb78563f57e2
carrington-manyuchi <carringtonmanyuchi263@gmail.com>
2020-02-14 12:23:06 +0200

    completed the home page first stage

--- pizzahouse/routes/web.php
+++ pizzahouse/routes/web.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function () {
+    /** return view('pizzas');
+    *** return 'pizzas'    ** We will be returning a string here
+    *** return ['name' => 'veg pizzas', 'base' => 'classic'];    ***returning an array
+
+    *** Below we get data from DB and pass that data as an array into the view
+    *** return view('pizzas', ['type' => 'hawaiian', 'base' => 'cheesy crust']); // means passing varable type into view & access that variable and output it
+    
+     
+    *** declare all values outside view method and just pass in as a variable
+    *** $pizza = [
+    ***    'type' => 'hawaiian' ,
+    ***    'base' => 'cheesy crust',
+    ***    'price' => 10      
+    ***    ];
+    **/
+
+    $pizzas = [
+        ['type' => 'hawaiian', 'base' => 'cheesy crust'],
+        ['type' => 'volcano', 'base' => 'garlic crust'],
+        ['type' => 'veg supreme', 'base' => 'thin & crispy ']    
+        ];
+
+    return view('pizzas', ['pizzas' => $pizzas]);
+
+
+});
